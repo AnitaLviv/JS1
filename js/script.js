282c248f3a9493863dcf1bcfd88ebb603adff59e
AnitaLviv <annet_v@ukr.net>
2016-11-21 13:38:17 +0200

    new

--- js/script.js
+++ js/script.js
@@ -1,21 +1,27 @@
-var a = prompt( 'Введите  число', '' );
-var b = prompt( 'Введите  степень', '' );
-
-function pow( a, b ) {
-  var result = 1;
-  
-  if (b < 0) {
-    return 1 / pow(a, -b);
-  }
-  
-  for( var i = 0; i < b; i++ ) {
-    result *= a;
-  }  
-  
-  return result;  
- }
 
 
-var bigResult = pow( a, b );
-console.log( bigResult );
+var arr = [];
+for (var i = 0; i < 5; i++) {
+    var names = prompt('Введите имя пользователя','');
+    arr.push(names);
+}
+console.log(arr);
 
+var userName = prompt('Введите Ваше имя', '');
+if (userName == '') {
+    alert("Попробуйте еще раз");
+}
+
+
+for (var index = 0; index < arr.length; index++) {
+    if(arr[index] == userName) {
+        var search = true;
+        break;
+    }
+}
+if (search) {
+    alert(userName + ", Вы успешно вошли");
+}
+else {
+    alert('Ошибка, неверное имя пользователя')
+}
