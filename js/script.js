e7b27f119903876b7b19b8bad560ea69a2fa1e9c
AnitaLviv <annet_v@ukr.net>
2016-11-26 15:51:03 +0200

    new

--- js/script.js
+++ js/script.js
@@ -1,27 +1,50 @@
+function enterNames() {
+ var arr = [];
 
+for (i = 0; i < 5; i++) {
+ arr[i] = prompt('Введите любое имя', '');
 
-var arr = [];
-for (var i = 0; i < 5; i++) {
-    var names = prompt('Введите имя пользователя','');
-    arr.push(names);
+ while (arr[i] === '') {
+    alert('Вы не указали имя');
+    arr[i] = prompt('Введите любое имя', '');
 }
+
+if (arr[i] === null) {
+    alert('Отмена');
+    return false;
+  }
+}
+
 console.log(arr);
+    
 
-var userName = prompt('Введите Ваше имя', '');
-if (userName == '') {
-    alert("Попробуйте еще раз");
-}
+var userName = prompt('Введите имя пользователя','');
+ for ( var i = 0; i < 5; i++ ) {
 
-
-for (var index = 0; index < arr.length; index++) {
-    if(arr[index] == userName) {
-        var search = true;
-        break;
+if (userName === null) {
+     alert('Отмена');
+       return false;
+        }
     }
+
+while (userName === '') {
+        alert('Вы не указали имя');
+        userName = prompt('Введите имя пользователя','');
+    }
+
+for (var j = 0; j < arr.length; j += 1) {
+        if (arr[j] === userName) {
+            alert(userName + ', Вы успешно вошли');
+            return;
 }
-if (search) {
-    alert(userName + ", Вы успешно вошли");
-}
-else {
-    alert('Ошибка, неверное имя пользователя')
-}
+
+if (arr[i] !== userName) {
+            alert('Ошибка, пользователя с таким именем нету!');
+            return;
+        }
+
+    }
+} 
+    
+    enterNames();
+
